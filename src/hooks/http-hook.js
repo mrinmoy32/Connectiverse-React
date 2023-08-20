f8205a4f3296fef4963b6268089122205a7d25f6
Mrinmoy Pal <mrinmoy21pal@gmail.com>
2023-08-20 22:55:47 +0530

    making work of Signup API

---
+++ src/hooks/http-hook.js
@@ -0,0 +1,54 @@
+import { useState, useCallback, useRef, useEffect } from "react";
+import axios from 'axios';
+
+export const useHttpClient = () => {
+  const [isLoading, setIsloading] = useState(false);
+  const [error, setError] = useState();
+
+  const activeHttpRequests = useRef([]);
+
+  const sendRequest = useCallback(
+    async (url, method = "GET", body = null, headers = {}) => {
+      setIsloading(true);
+      const source = axios.CancelToken.source();
+      activeHttpRequests.current.push(source);
+
+      try {
+        setError(null);
+        const response = await axios({
+          method,
+          url,
+          data: body,
+          headers,
+          cancelToken: source.token,
+        });
+
+        const responseData = response.data;
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          (cancelToken) => cancelToken !== source
+        );
+
+        setIsloading(false);
+        return responseData;
+      } catch (error) {
+        setIsloading(false);
+        setError(error.message || "Something went wrong, please try again");
+        throw error;
+      }
+    },
+    []
+  );
+
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      activeHttpRequests.current.forEach((cancelToken) => cancelToken.cancel());
+    };
+  }, []);
+
+  return { isLoading, error, sendRequest, clearError };
+};
--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,33 +1,124 @@
-import React from 'react';
-import './SignupPage.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useHttpClient } from "../hooks/http-hook";
+import "./SignupPage.css";
+import { Link } from "react-router-dom";
+import LoadingSpinner from "../components/common/LoadingSpinner";
+import ErrorModal from "../components/common/ErrorModal";
 
 function SignupPage() {
+  const initialFormState = {
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  };
+  const navigate = useNavigate();
+  const [formState, setFormState] = useState(initialFormState);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+
+  const signupHandler = async (event) => {
+    event.preventDefault();
+    try {
+      const responseData = await sendRequest(
+        "http://demoyourprojects.com:5085/auth/signup",
+        "POST",
+        JSON.stringify({
+          name: formState.name,
+          email: formState.email,
+          password: formState.password,
+          confirm_password: formState.confirmPassword,
+        }),
+        {
+          "Content-Type": "application/json",
+        }
+      );
+      console.log(responseData.data.user);
+      window.alert("Signup successful!");
+      navigate("/user/profile/");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
-    <div className="signup-page">
-      <div className="singup__left-section">
-        <div className="signup-text">
-          <h1>Join Connectiverse</h1>
-          <p>Create your account and connect with others</p>
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && <LoadingSpinner asOverlay />}
+      <div className="signup-page">
+        <div className="singup__left-section">
+          <div className="signup-text">
+            <h1>Join Connectiverse</h1>
+            <p>Create your account and connect with others</p>
+          </div>
+        </div>
+        <div className="signup__right-section">
+          <form className="signup-form">
+            <h2>Welcome aboard</h2>
+            <div className="input-container">
+              <input
+                label="name"
+                type="text"
+                placeholder="Name"
+                required
+                onChange={(event) => {
+                  setFormState({
+                    ...formState,
+                    name: event.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                label="email"
+                type="email"
+                placeholder="Email address"
+                required
+                onChange={(event) => {
+                  setFormState({
+                    ...formState,
+                    email: event.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                label="password"
+                type="password"
+                placeholder="Password"
+                required
+                onChange={(event) => {
+                  setFormState({
+                    ...formState,
+                    password: event.target.value,
+                  });
+                }}
+              />
+            </div>
+            <div className="input-container">
+              <input
+                label="confirmPassword"
+                type="password"
+                placeholder="Confirm password"
+                required
+                onChange={(event) => {
+                  setFormState({
+                    ...formState,
+                    confirmPassword: event.target.value,
+                  });
+                }}
+              />
+            </div>
+            <button className="signup-button" onClick={signupHandler}>
+              Sign up
+            </button>
+            <Link to="/login">Already have an account? plesae login</Link>
+          </form>
         </div>
       </div>
-      <div className="signup__right-section">
-        <form className="signup-form">
-          <h2>Welcome aboard</h2>
-          <div className="input-container">
-            <input type="text" placeholder="Name" required/>
-          </div>
-          <div className="input-container">
-            <input type="email" placeholder="Email address" required/>
-          </div>
-          <div className="input-container">
-            <input type="password" placeholder="Password" required/>
-          </div>
-          <Link to="/user/profile"><button className="signup-button">Sign up</button></Link>
-          <Link to="/login">Already have an account? plesae login</Link>
-        </form>
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
